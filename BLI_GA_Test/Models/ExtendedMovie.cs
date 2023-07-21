13fb5c074c92ef8b2450064429f145ca7df67ea3
omid-94 <omiddadvar94@gmail.com>
2023-07-21 22:45:29 +0330

    Extended movies Added => Predicted Rating...

---
+++ BLI_GA_Test/Models/ExtendedMovie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLI_GA_Test.Models
+{
+    internal class ExtendedMovie : Movie
+    {
+        public ExtendedMovie() : base()
+        {
+            Rating = -1;
+        }
+        public double Rating { get; set; }
+    }
+}
